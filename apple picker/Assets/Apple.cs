04d2a14524ebd7f96e4a0754bbabfd6f6453215f
Yicheng Wang <wyc25013@gmail.com>
2015-11-05 00:01:20 -0500

    apple picker

---
+++ apple picker/Assets/Apple.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : MonoBehaviour {
+	public static float     bottomY = -20f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( transform.position.y < bottomY ) {
+			Destroy( this.gameObject );
+
+			// Get a reference to the ApplePicker component of Main Camera
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>(); // 1
+			// Call the public AppleDestroyed() method of apScript
+			apScript.AppleDestroyed();                                      // 2
+		}
+
+	}
+}
---
+++ apple picker/Assets/HighScore.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+	static public int    score = 1000;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake() {                                                          // 1
+		// If the ApplePickerHighScore already exists, read it
+		if (PlayerPrefs.HasKey("ApplePickerHighScore")) {                   // 2
+			score = PlayerPrefs.GetInt("ApplePickerHighScore");
+		}
+		// Assign the high score to ApplePickerHighScore
+		PlayerPrefs.SetInt("ApplePickerHighScore", score);                  // 3
+	}
+
+	// Update is called once per frame
+	void Update () {
+		TextMesh gt = this.GetComponent<TextMesh>();
+		gt.text = "High Score: "+score;
+		// Update ApplePickerHighScore in PlayerPrefs if necessary
+		if (score > PlayerPrefs.GetInt("ApplePickerHighScore")) {           // 4
+			PlayerPrefs.SetInt("ApplePickerHighScore", score);
+		}
+	}
+}
