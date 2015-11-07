c9c89165e02f1d2139b5b69628616457adaa7446
Yicheng Wang <wyc25013@gmail.com>
2015-11-07 15:27:20 -0500

    two players

---
+++ iPong/Assets/bracketCtr1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bracketCtr1 : MonoBehaviour {
+
+	public float bracketSpeed = 1;
+	public Vector3 playerPos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float yPos = gameObject.transform.position.y + (Input.GetAxis ("Vertical1") * bracketSpeed);
+		float xPos = gameObject.transform.position.x + (Input.GetAxis ("Horizontal1") * bracketSpeed);
+		playerPos = new Vector3 (Mathf.Clamp(xPos, -5, 0), Mathf.Clamp (yPos, -11, 11), 0);
+		gameObject.transform.position = playerPos;
+	}
+}
---
+++ iPong/Assets/bracketCtr2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bracketCtr2 : MonoBehaviour {
+	
+	public float bracketSpeed = 1;
+	public Vector3 playerPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float yPos = gameObject.transform.position.y + (Input.GetAxis ("Vertical2") * bracketSpeed);
+		float xPos = gameObject.transform.position.x + (Input.GetAxis ("Horizontal2") * bracketSpeed);
+		playerPos = new Vector3 (Mathf.Clamp(xPos, 0, 5), Mathf.Clamp (yPos, -11, 11), 0);
+		gameObject.transform.position = playerPos;
+	}
+}
